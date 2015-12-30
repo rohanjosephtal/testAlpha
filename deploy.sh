ada6e0afc9ca812a43a00436b76d76ba8dd7b49e
rohanjoseph <rohan.joseph89@gmail.com>
2015-12-30 13:25:44 +0530

    changes

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-exec sudo touch /tmp/credentials
-echo "[default]" >> /tmp/credentials
-
-echo "aws_access_key_id"=$AWS_ACCESS_KEY_ID
-echo "aws_secret_access_key"=$AWS_SECRET_ACCESS_KEY
-
-echo cat /tmp/credentials
